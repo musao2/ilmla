aafce70d4d73be5dd0f63f137c561ef963fdd500
Furkatov0701 <furqatovzabihulloh@gmail.com>
2026-02-10 17:40:54 +0500

    yangilanish

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,14 @@
 import Features from "./zabihulloh/Features"
 import Header from './components/Header'
 import Hero from './components/Hero'
+import Tadbir from "./zabihulloh/Tadbir"
 function App(){
   return(
     <>
     <Header/>
     <Hero/>
     <Features />
-
+    <Tadbir/>
     </>
   )
 }
---
+++ src/zabihulloh/Tadbir.jsx
@@ -0,0 +1,22 @@
+import ustozlar from "../assets/event.webp"
+
+export default function Tadbir() {
+    return (
+        <>
+        <div className="flex flex-row gap-20 px-15 py-10 bg-yellow-300 rounded-3xl mx-15">
+            <div className="flex flex-col gap-6 ">
+                <h1 className="font-bold text-3xl ">Studentlar uchun maxsus tashkillashtiriladigan tadbirlar</h1>
+                <p className="text-xl">Mashxur inflyunserlar va muvaffaqiyatli biznesmenlar bilan uchrashuvlar</p>
+                <p className="text-xl">Bepul IELTS CD Mock imtihonlar va har hafta maxsus Speaking klublari</p>
+                <button className="py-4 text-xl text-gray-390 rounded-xl  bg-white">Tadbirlarni Ko`rish</button>
+            </div>
+            <div>
+                <img
+                className="absolute right-15 top-[1370px]  w-80 "
+                src={ustozlar} 
+                alt="" />
+            </div>
+        </div>
+        </>
+    )
+}
